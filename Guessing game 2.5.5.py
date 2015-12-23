1e2ad82a0c13b4d117746be053e8b593526fa23a
Cam Barratt <barrattcb@gmail.com>
2015-12-23 11:55:39 +0200

    Fixed most bugs

--- Guessing game 2.5.5.py
+++ Guessing game 2.5.5.py
@@ -14,7 +14,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-
+#
 #imports the random mod and time
 import time
 import random
